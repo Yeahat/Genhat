7e769b0a3841ba4aa14e28e3694d4a908f762ce5
David Kent <davidkent@wpi.edu>
2013-07-10 21:09:03 -0400

    Thing Rendering and Example
    Added new Things:
    	Stairs - Allows Agents stepping on them to increase or decrease their
    z-coordinate depending on direction stepped and direction of the Stairs
    
    Added rendering for Things, of which Stairs are an example. 
    Still coming is an adjustment to the Step action that makes stairs have
    an actual effect on movement (right now they are simply a rendered image
    of stairs that any Agent can step on as if they were a regular terrain
    tile)

--- src/things/Thing.java
+++ src/things/Thing.java
@@ -1,8 +1,37 @@
 package things;
 
+import org.newdawn.slick.opengl.Texture;
+
+import entities.Agent.direction;
+
 public abstract class Thing {
+	//State
 	boolean blocking;
 	boolean crossable;
+	boolean ramp;
+	int[] pos = new int[3]; //Position (x, y, z)
+	direction dir;	//direction the object is facing
+	
+	//Texture
+	Texture texture;
+	int texRow;
+	int texCol;
+	protected int TEXTURE_SIZE_X = 24;	//Sprite sheet character width, this can be overridden
+	protected int TEXTURE_SIZE_Y = 32;	//Sprite sheet character height, this can be overridden
+	protected int TEXTURE_SHEET_WIDTH = 256; //Sprite sheet height, this can be overridden (must be a multiple of 2)
+	protected int TEXTURE_SHEET_HEIGHT = 256; //Sprite sheet width, this can be overridden (must be a multiple of 2)
+	
+	/**
+	 * Load any required textures for displaying
+	 */
+	public abstract void loadTextures();
+	
+	/**
+	 * Render the Thing with OpenGL
+	 * @param pixelSize the ratio of real screen pixels to in game pixels
+	 * @param terrainTextureSize the pixel dimensions of a terrain tile
+	 */
+	public abstract void renderThing(int pixelSize, int terrainTextureSize);
 	
 	/**
 	 * Getter for blocking
@@ -20,5 +49,15 @@ public abstract class Thing {
 	public boolean isCrossable()
 	{
 		return crossable;
+	}
+	
+	/**
+	 * Getter for whether or not something is a ramp (i.e. an agent walking on it will change
+	 * the (x,y) position and the z position)
+	 * @return true if it is a ramp
+	 */
+	public boolean isRamp()
+	{
+		return ramp;
 	}
 }
