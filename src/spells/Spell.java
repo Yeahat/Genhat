6835f4325c4b566a1943d3749cd320fa349951c8
Ethan <ethanwolfe90@gmail.com>
2013-07-16 19:43:59 -0400

    Added skeletons for Spell and SpellEffect

--- src/spells/Spell.java
+++ src/spells/Spell.java
@@ -1,5 +1,30 @@
 package spells;
 
+import java.util.ArrayList;
+
+import entities.Agent;
+
 public class Spell {
 
+	
+	private String name;
+	private ArrayList<SpellEffect> effects;
+	
+	public Spell(){
+		
+	}
+	
+	public void applyEffect(Agent target){
+		for(SpellEffect effect : effects){
+			effect.applyEffect(target);
+		}
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public ArrayList<SpellEffect> getEffects(){
+		return effects;
+	}
 }
--- src/spells/SpellEffect.java
+++ src/spells/SpellEffect.java
@@ -1,5 +1,14 @@
 package spells;
+import entities.Agent;
 
 public class SpellEffect {
-
+	
+	public SpellEffect(){
+		//provide delta variables to represent buffs and debuffs
+	}
+	
+	public void applyEffect(Agent target){
+		//apply the affect to the target
+		//need to work out how this will effect land
+	}
 }
