0ee088a771370190c05a88ab4e3e84e18959b21f
David Kent <davidkent@wpi.edu>
2014-01-17 20:56:27 -0500

    Added a path following action that will either follow a path once or follow a path on a loop.

---
+++ src/actions/FollowPath.java
@@ -0,0 +1,123 @@
+package actions;
+
+import java.util.ArrayList;
+
+import world.World;
+import entities.Agent;
+
+import static entities.Agent.direction.*;
+
+public class FollowPath implements Action 
+{	
+	String path;
+	int frequency;
+	boolean looping;
+	boolean executingStep;
+	char currentStep;
+
+	Step step;
+	ArrayList<String> stepArgs;
+	
+	public FollowPath()
+	{
+		frequency = 0;
+		step = new Step();
+		path = "";
+		looping = false;
+		executingStep = false;
+		currentStep = '-';
+		stepArgs = new ArrayList<String>();
+	}
+	
+	public FollowPath(boolean isLooping, int freq)
+	{
+		frequency = freq;
+		step = new Step();
+		path = "";
+		looping = isLooping;
+		executingStep = false;
+		currentStep = '-';
+		stepArgs = new ArrayList<String>();
+	}
+	
+	@Override
+	public void execute(Agent agent, World world, ArrayList<String> args) 
+	{
+		if (currentStep == '-' && path.length() == 0)
+			return;
+		
+		if (currentStep == '-')
+		{
+			if (looping)
+			{
+				currentStep = path.charAt(0);
+				path += path.charAt(0);
+				path = path.substring(1);
+			}
+			else
+			{
+				currentStep = path.charAt(0);
+				if (path.length() == 1)
+					path = "";
+				else
+					path = path.substring(1);
+			}
+			
+			//handle invalid strings in the path
+			if (currentStep != 'u' || currentStep != 'd' || currentStep != 'l' || currentStep != 'r')
+			{
+				currentStep = '-';
+			}
+			else
+			{
+				stepArgs.clear();
+				switch (currentStep)
+				{
+				case 'u':	stepArgs.add("up");		break;
+				case 'd':	stepArgs.add("down");	break;
+				case 'l':	stepArgs.add("left");	break;
+				case 'r':	stepArgs.add("right");	break;
+				}
+				executingStep = false;
+			}
+		}
+		
+		step.execute(agent, world, stepArgs);
+		if (step.isFinished() && !executingStep)
+		{
+			//step is blocked by something, try again on the next cycle
+		}
+		else if (!executingStep)
+		{
+			executingStep = true;
+		}
+		else if (executingStep && step.isFinished())
+		{
+			executingStep = false;
+			currentStep = '-';
+		}
+	}
+
+	@Override
+	public boolean isFinished() {
+		if (currentStep == '-' && path.length() == 0)
+			return true;
+		else
+			return false;
+	}
+
+	public void setLooping(boolean isLooping)
+	{
+		looping = isLooping;
+	}
+	
+	public void setFrequency(int freq)
+	{
+		frequency = freq;
+	}
+	
+	public void setPath(String newPath)
+	{
+		path = newPath.toLowerCase();
+	}
+}
