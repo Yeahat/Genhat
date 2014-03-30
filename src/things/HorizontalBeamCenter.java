05d0da44074428040b8fb51a6d96daa24ef9999e
David Kent <davidkent@wpi.edu>
2014-03-30 00:15:04 -0400

    Added a bunch of new Things, including bars (tables) and wooden beams (building support). They still need to be fully tested, though!

---
+++ src/things/HorizontalBeamCenter.java
@@ -0,0 +1,87 @@
+package things;
+
+import java.io.IOException;
+
+import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.ResourceLoader;
+
+import entities.Agent.direction;
+import static entities.Agent.direction.*;
+
+public class HorizontalBeamCenter extends Thing {
+
+	public HorizontalBeamCenter()
+	{
+		texRow = 1;
+		texCol = 1;
+		blocking = false;
+		transparent = true; //true if thing should not block light sources
+		setDir(up);	//direction the thing is facing
+	}
+	
+	public HorizontalBeamCenter(direction d)
+	{
+		texRow = 1;
+		texCol = 1;
+		blocking = false;
+		transparent = true; //true if thing should not block light sources
+		setDir(d);	//direction the thing is facing
+	}
+	
+	@Override
+	public void loadTextures() {
+		try {
+			texture = TextureLoader.getTexture("png", ResourceLoader.getResourceAsStream("graphics/objects/thing1.png"));
+		} catch (IOException e) {e.printStackTrace();}
+	}
+
+	@Override
+	public void renderThing(int pixelSize, int terrainTextureSize) {
+		GL11.glPushMatrix();
+		
+			texture.bind();
+			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
+		
+			float tConvX = ((float)TEXTURE_SIZE_X)/((float)TEXTURE_SHEET_WIDTH);
+			float tConvY = ((float)TEXTURE_SIZE_Y)/((float)TEXTURE_SHEET_HEIGHT);
+			
+			int texX = texCol * 4;
+			int texY = texRow;
+			switch (getDir())
+			{
+			case left:
+				texX += 0;
+				break;
+			case down:
+				texX += 1;
+				break;
+			case right:
+				texX += 2;
+				break;
+			case up:
+				texX += 3;				
+				break;
+			}
+			
+			int xMin = pixelSize * ((terrainTextureSize - TEXTURE_SIZE_X) / 2);
+			int xMax = xMin + pixelSize * (TEXTURE_SIZE_X);
+			int yMin = 0;
+			int yMax = yMin + pixelSize * (TEXTURE_SIZE_Y);
+			
+			GL11.glBegin(GL11.GL_QUADS);
+				GL11.glTexCoord2f(texX * tConvX, texY*tConvY + tConvY);
+				GL11.glVertex2f(xMin, yMin);
+				GL11.glTexCoord2f(texX*tConvX + tConvX, texY*tConvY + tConvY);
+				GL11.glVertex2f(xMax, yMin);
+				GL11.glTexCoord2f(texX*tConvX + tConvX, texY * tConvY);
+				GL11.glVertex2f(xMax, yMax);
+				GL11.glTexCoord2f(texX*tConvX, texY * tConvY);
+				GL11.glVertex2f(xMin, yMax);
+			GL11.glEnd();
+			
+		GL11.glPopMatrix();
+	}
+
+}
